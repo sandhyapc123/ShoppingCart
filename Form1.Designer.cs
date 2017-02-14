f74f982375a073ef7ef0170214d664088553bac5
sandhyapc123 <sandhyapc123@gmail.com>
2017-02-14 16:11:00 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ShoppingCart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.appleBox = new System.Windows.Forms.TextBox();
+            this.orangesBox = new System.Windows.Forms.TextBox();
+            this.totalCostBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(191, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(542, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome To Apples And Oranges";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(191, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(283, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please Enter the Count";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(176, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apples";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(176, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Oranges";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(176, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 31);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total Cost";
+            // 
+            // appleBox
+            // 
+            this.appleBox.Location = new System.Drawing.Point(399, 200);
+            this.appleBox.Name = "appleBox";
+            this.appleBox.Size = new System.Drawing.Size(100, 22);
+            this.appleBox.TabIndex = 5;
+            this.appleBox.TextChanged += new System.EventHandler(this.appleBox_TextChanged);
+            // 
+            // orangesBox
+            // 
+            this.orangesBox.Location = new System.Drawing.Point(399, 259);
+            this.orangesBox.Name = "orangesBox";
+            this.orangesBox.Size = new System.Drawing.Size(100, 22);
+            this.orangesBox.TabIndex = 6;
+            this.orangesBox.TextChanged += new System.EventHandler(this.orangesBox_TextChanged);
+            // 
+            // totalCostBox
+            // 
+            this.totalCostBox.Location = new System.Drawing.Point(399, 325);
+            this.totalCostBox.Name = "totalCostBox";
+            this.totalCostBox.ReadOnly = true;
+            this.totalCostBox.Size = new System.Drawing.Size(100, 22);
+            this.totalCostBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 518);
+            this.Controls.Add(this.totalCostBox);
+            this.Controls.Add(this.orangesBox);
+            this.Controls.Add(this.appleBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Checkout";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox appleBox;
+        private System.Windows.Forms.TextBox orangesBox;
+        private System.Windows.Forms.TextBox totalCostBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoppingCart
+{
+    public partial class Form1 : Form
+    {
+        Double AppleCost = 0.45;
+        Double OrangeCost = 0.65;
+        Double AppleCount = 0.0;
+        Double OrangeCount = 0.0;
+        Double cartTotal = 0.0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void appleBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (appleBox.Text != string.Empty)
+                {
+                    AppleCount = Convert.ToDouble(appleBox.Text);
+                }
+                else
+                {
+                    AppleCount = 0;
+                }
+                cartTotalCalc(AppleCount, OrangeCount);
+                totalCostBox.Text = cartTotal.ToString();
+            }
+            catch (Exception ex)
+            {
+                //write to log and system trace
+            }
+        }
+
+        private void orangesBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (orangesBox.Text != string.Empty)
+                {
+                    OrangeCount = Convert.ToDouble(orangesBox.Text);
+                }
+                else
+                {
+                    OrangeCount = 0;
+                }
+                cartTotalCalc(AppleCount, OrangeCount);
+                totalCostBox.Text = cartTotal.ToString();
+            }
+            catch (Exception ex)
+            {
+                //write to log and system trace
+            }
+        }
+
+        private void cartTotalCalc(Double apples, Double oranges)
+        {
+            cartTotal = 0;
+            if (AppleCount != 0.0)
+            {                
+                cartTotal = AppleCost * AppleCount;
+            }
+            if (OrangeCount != 0.0)
+            {               
+                cartTotal = cartTotal + OrangeCost * OrangeCount;
+            }
+        }
+
+       
+    }
+}
